b4eb4979b301d6560f64b5b0775e24fdb9676cf3
noatuh <noatuh@outlook.com>
2025-04-12 04:59:43 -0400

    updated port to 5050

--- server.py
+++ server.py
@@ -176,4 +176,4 @@ def clear_measurements():
     return jsonify({"status": "cleared"})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5050)
